41119124bbaed70699a2c2272ae6cecec3ccd7ed
ronanpnc <ronan.ogor@passerellesnumeriques.org>
2021-11-21 10:04:13 +0700

    removed back - to be replaced by laravel

--- back/server.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const app = express();
-
-const SERVER_PORT = 3000;
-app.listen(SERVER_PORT, () => console.log("listening on : " + SERVER_PORT));
-
-app.use(express.static("../front/dist"));
